9510e5ceff65c809e139492d00c6b8b34c20ddb1
yanwarmtb <yanwarmtb@gmail.com>
2025-09-19 17:04:29 +0700

    Batch replace: "mrdickymiswardi" -> "yanwarmtb" | getAudio.js | hits=1

--- functions/api/getAudio.js
+++ functions/api/getAudio.js
@@ -8,7 +8,7 @@ const CORS = {
   "Access-Control-Allow-Headers": "Content-Type, Authorization, Range",
 };
 
-const OWNER_REPO = "mrdickymiswardi/server";
+const OWNER_REPO = "yanwarmtb/server";
 const BRANCH = "main";
 const DIR = "audio";
 
