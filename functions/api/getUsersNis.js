d1efcbc89af30973c607ae988601885e1c06594b
yanwarmtb <yanwarmtb@gmail.com>
2025-09-19 17:04:46 +0700

    Batch replace: "mrdickymiswardi" -> "yanwarmtb" | getUsersNis.js | hits=1

--- functions/api/getUsersNis.js
+++ functions/api/getUsersNis.js
@@ -1,7 +1,7 @@
 // functions/api/getUsersNis.js
 // Endpoint: GET /api/getUsersNis
 
-const DEFAULT_REPO = "mrdickymiswardi/server";
+const DEFAULT_REPO = "yanwarmtb/server";
 const USERS_PATH   = "user.json";
 
 const dec = new TextDecoder();
