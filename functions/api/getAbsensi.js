c16ff6a7ad7bbfd5f2c0d7db9fb44f8eb8a7a9ee
yanwarmtb <yanwarmtb@gmail.com>
2025-09-19 17:04:23 +0700

    Batch replace: "mrdickymiswardi" -> "yanwarmtb" | getAbsensi.js | hits=1

--- functions/api/getAbsensi.js
+++ functions/api/getAbsensi.js
@@ -1,7 +1,7 @@
 // functions/api/getAbsensi.js
 // Endpoint: GET /api/getAbsensi?kelas=KELAS&tanggal=YYYY-MM-DD
 
-const DEFAULT_REPO   = "mrdickymiswardi/server";
+const DEFAULT_REPO   = "yanwarmtb/server";
 const DEFAULT_BRANCH = "main";
 
 const dec = new TextDecoder();
