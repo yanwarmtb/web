dc4c8913d4137c5695caeb77fa1ea9b8a9bc9da5
yanwarmtb <yanwarmtb@gmail.com>
2025-09-19 17:04:15 +0700

    Batch replace: "mrdickymiswardi" -> "yanwarmtb" | cekAdminWali.js | hits=1

--- functions/api/cekAdminWali.js
+++ functions/api/cekAdminWali.js
@@ -9,7 +9,7 @@ const CORS = {
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
-const OWNER_REPO = "mrdickymiswardi/server";
+const OWNER_REPO = "yanwarmtb/server";
 const BRANCH = "main";
 const SECURE_PATH = "secureWali.json";
 
