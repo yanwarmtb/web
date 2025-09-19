80e5acf6b70af316ed6c152e38cc4e340b8fc832
yanwarmtb <yanwarmtb@gmail.com>
2025-09-19 17:04:56 +0700

    Batch replace: "mrdickymiswardi" -> "yanwarmtb" | tambahSantri.js | hits=1

--- functions/api/tambahSantri.js
+++ functions/api/tambahSantri.js
@@ -1,7 +1,7 @@
 // functions/api/tambahSantri.js
 // Endpoint: POST /api/tambahSantri
 
-const DEFAULT_REPO   = "mrdickymiswardi/server";
+const DEFAULT_REPO   = "yanwarmtb/server";
 const DEFAULT_BRANCH = "main";
 
 const enc = new TextEncoder();
